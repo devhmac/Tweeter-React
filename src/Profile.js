c31639c15f6f891ca392b36ff7ea0b7b9f7a653b
Devin MacGillivray <devhmac@gmail.com>
2021-04-10 17:36:43 -0600

    profile, tweetform and tweet components added

---
+++ src/Profile.js
@@ -0,0 +1,15 @@
+import React from 'react'
+export const Profile = () => {
+
+  return (
+    <header>
+      <div>
+        <img src="../images/profile-hex.png" />
+      </div>
+      <br />
+      <div>
+        <h2>Devin MacGillivray</h2>
+      </div>
+    </header>
+  )
+}
