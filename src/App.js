8780e61c1e0336da5b197d848ec8d4b79a96273b
Devin MacGillivray <devhmac@gmail.com>
2021-04-10 16:55:58 -0600

    navigation.js init

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
 import './App.css';
 
+const Navigation = () => {
+
+  return
+}
+
+const Profile = () => {
+
+  return
+}
+
+const TweetForm = () => {
+
+  return
+}
+
+const Tweet = () => {
+
+  return
+}
+
 function App() {
   return (
     <div className="App">
